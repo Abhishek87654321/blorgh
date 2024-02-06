eef3151b3b110036a2adc8b059a531e9cb5a96c2
Abhishek87654321 <abhishekgangdhari@gmail.com>
2024-02-06 10:57:59 +0530

    article scaffold add

--- config/routes.rb
+++ config/routes.rb
@@ -1,2 +1,11 @@
 Blorgh::Engine.routes.draw do
+    root to: "articles#index"
+    # localhost:3000/articles
+    # because this is a engine, and it is named blorgh
+    # this becomes :after => "localhost:3000/articles"
+
+    # This creates a nested route for the comments, which is what the form requires.
+    resources :articles do
+        resources :comments
+    end
 end
